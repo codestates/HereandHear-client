9e29035829fe7dcb0dc5310cd2b394cb7e2f0b2d
basilry <basilry@gmail.com>
2021-06-09 11:43:19 +0900

    routes

---
+++ src/pages/SignUp.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+
+function SignUp() {
+
+  const [ id, setId ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ password2, setPassword2 ] = useState('');
+
+  const handleId = (e) => {
+    setId(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handlePassword2 = (e) => {
+    setPassword2(e.target.value)
+  }
+
+  const signUpBtn = () => {
+    if(id === '' || password === '' || password2 === '') {
+      alert('아이디나 패스워드를 입력해주세요')
+      // return;
+    } else if(password !== password2) {
+      alert('동일한 비밀번호를 입력해주세요')
+      // return;
+    } else {
+      alert('회원가입이 되셨습니다!')
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <ul>
+          <li>signUp</li>
+          <li><input value={id} onChange={handleId} ></input></li>
+          <li><input type="password" value={password} onChange={handlePassword}></input></li>
+          <li><input type="password" value={password2} onChange={handlePassword2}></input></li>
+          <li>이용약관 동의합니다</li>
+          <li><button onClick={signUpBtn}>signUp</button></li>
+        </ul>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp;
